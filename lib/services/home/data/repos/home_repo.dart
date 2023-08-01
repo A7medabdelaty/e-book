5d4214fa5a121e09f092d0e4cb91c4c93f3db23e
A7medabdelaty <elqahwagy.123@gmail.com>
2023-08-02 00:21:38 +0300

    add api service class

---
+++ lib/services/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/services/home/data/models/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo{
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+}
