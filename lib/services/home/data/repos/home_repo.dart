b62a29aaf7914d205a9ff2d0341558370bfb6bbf
A7medabdelaty <elqahwagy.123@gmail.com>
2023-08-05 22:40:15 +0300

    implement fetch featured books function

--- lib/services/home/data/repos/home_repo.dart
+++ lib/services/home/data/repos/home_repo.dart
@@ -2,7 +2,9 @@ import 'package:bookly/core/errors/failures.dart';
 import 'package:bookly/services/home/data/models/book_model.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class HomeRepo{
+abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks(
+      {required String query});
 }
