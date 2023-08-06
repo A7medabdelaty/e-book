52d2ceda5abacc605b7e5b7fa660460f19cb0a64
A7medabdelaty <elqahwagy.123@gmail.com>
2023-08-06 22:21:20 +0300

    create service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/service_locator.dart';
 import 'package:bookly/services/details/presentation/view/details_view.dart';
+import 'package:bookly/services/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/services/home/presentation/view%20model/featured%20books%20cubit/featured_books_cubit.dart';
+import 'package:bookly/services/home/presentation/view%20model/newest%20books%20cubit/newest_books_cubit.dart';
 import 'package:bookly/services/home/presentation/views/home_view.dart';
 import 'package:bookly/services/search/presentation/views/search_view.dart';
 import 'package:bookly/services/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -15,19 +20,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      theme: ThemeData.dark(useMaterial3: true).copyWith(
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
-        scaffoldBackgroundColor: kMainColor,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>())),
+        BlocProvider(
+            create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>())),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.dark,
+        theme: ThemeData.dark(useMaterial3: true).copyWith(
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+          scaffoldBackgroundColor: kMainColor,
+        ),
+        routes: {
+          HomeView.routeName: (context) => const HomeView(),
+          DetailsView.routeName: (context) => const DetailsView(),
+          SearchView.routeName: (context) => const SearchView(),
+        },
+        home: const SplashView(),
       ),
-      routes: {
-        HomeView.routeName: (context) => const HomeView(),
-        DetailsView.routeName:(context) => const DetailsView(),
-        SearchView.routeName:(context) => const SearchView(),
-      },
-      home: const SplashView(),
     );
   }
 }
